45737819655be0cf90c6cc85153cdfbc5a3da69f
Dogukan Cifci <dogukancifcii5@gmail.com>
2024-05-18 19:09:03 +0300

    Bootstrap and Navbar

--- techcareer-fullstack-frontend/src/layout/Navbar.js
+++ techcareer-fullstack-frontend/src/layout/Navbar.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div>
+      <nav
+        className="navbar bg-dark border-bottom border-body"
+        data-bs-theme="dark"
+      >
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            Todo List
+          </a>
+          <button className="btn btn-outline-light" to="/addtodo">
+            Add Todo
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
