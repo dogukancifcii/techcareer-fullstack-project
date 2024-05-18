bbf610bf81b231cbe5d223a1b400661f685de298
Dogukan Cifci <dogukancifcii5@gmail.com>
2024-05-18 19:58:11 +0300

    Action Buttons

--- techcareer-fullstack-frontend/src/pages/Home.js
+++ techcareer-fullstack-frontend/src/pages/Home.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { GoCircle, GoCheckCircleFill } from "react-icons/go";
+import { AiFillEdit } from "react-icons/ai";
+import { FaTrash } from "react-icons/fa";
+import { Link } from "react-router-dom";
 function Home() {
   //Butun todolar burada
   const [todos, setTodos] = useState([]);
@@ -14,6 +18,23 @@ function Home() {
     setTodos(result.data);
   };
 
+  //check complated todo
+  const completeTodo = async (id, todo) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id
+          ? {
+              ...todo,
+              completed: !todo.completed,
+            }
+          : todo
+      )
+    );
+    await axios.put(`http://localhost:8080/todo/${id}`, {
+      ...todo,
+      completed: !todo.completed,
+    });
+  };
   return (
     <div className="container ">
       <div className="py-4">
@@ -28,11 +49,44 @@ function Home() {
           </thead>
           <tbody>
             {todos.map((todo, index) => (
-              <tr className="" key={index}>
+              <tr key={index}>
                 <th scope="row">{index + 1}</th>
-                <td>{todo.title}</td>
-                <td>{todo.contents}</td>
-                <td></td>
+                <td
+                  className={
+                    todo.completed
+                      ? "text-danger text-decoration-line-through"
+                      : ""
+                  }
+                >
+                  {todo.title}
+                </td>
+                <td
+                  className={
+                    todo.completed
+                      ? "text-danger text-decoration-line-through"
+                      : ""
+                  }
+                >
+                  {todo.contents}
+                </td>
+                <td>
+                  <button
+                    onClick={() => completeTodo(todo.id, todo)}
+                    className="btn btn-secondary mx-2 fs-5"
+                  >
+                    {todo.completed ? <GoCheckCircleFill /> : <GoCircle />}
+                  </button>
+                  <Link
+                    to={`/edittodo/${todo.id}`}
+                    className="btn btn-warning mx-2 fs-5"
+                  >
+                    <AiFillEdit className="me-2" />
+                    Edit
+                  </Link>
+                  <button className="btn btn-danger mx-2 fs-5">
+                    <FaTrash className="me-2" /> Delete
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
