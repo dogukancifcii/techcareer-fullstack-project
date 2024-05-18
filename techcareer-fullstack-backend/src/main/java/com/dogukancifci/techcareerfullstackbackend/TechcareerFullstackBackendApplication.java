2ef8b2718ec628b9bfe9ce53f6fe3284f0f0ac32
Dogukan Cifci <dogukancifcii5@gmail.com>
2024-05-18 12:41:36 +0300

    Backend springboot folder

---
+++ techcareer-fullstack-backend/src/main/java/com/dogukancifci/techcareerfullstackbackend/TechcareerFullstackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dogukancifci.techcareerfullstackbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechcareerFullstackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechcareerFullstackBackendApplication.class, args);
+	}
+
+}
---
+++ techcareer-fullstack-backend/src/test/java/com/dogukancifci/techcareerfullstackbackend/TechcareerFullstackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogukancifci.techcareerfullstackbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechcareerFullstackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
