a6641fb8cec00140cef9e65bfdeb1cdea3c1c0e8
Dogukan Cifci <dogukancifcii5@gmail.com>
2024-05-18 20:41:23 +0300

    AddTodo Component

--- techcareer-fullstack-frontend/src/todos/AddTodo.js
+++ techcareer-fullstack-frontend/src/todos/AddTodo.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function AddTodo() {
+  let navigate = useNavigate();
+  const [todo, setTodo] = useState({
+    title: "",
+    contents: "",
+  });
+
+  //deconstruct yaptik burada yani suslu parantez disina cikardik
+  const { title, contents } = todo;
+
+  const handleChange = (e) => {
+    setTodo({ ...todo, [e.target.name]: e.target.value });
+    //e.target.title title ozelligi ile hangi todo ogesini secer. e.target.value ise input degeri alir
+    //...todo amaci onceki todoyu kopyala suslu parantez sıkıntı cikarmasin
+  };
+
   return (
-    <div>AddTodo</div>
-  )
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+          <h2 className="text-center m-4">Add Todo</h2>
+          <form>
+            <div className="mb-3">
+              <label htmlFor="title" className="form-label">
+                Title
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your title"
+                name="title"
+                value={title}
+                onChange={(e) => handleChange(e)}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="contents" className="form-label">
+                Content
+              </label>
+              <input
+                type={"text"}
+                className="form-control"
+                placeholder="Enter your content"
+                name="contents"
+                value={contents}
+                onChange={(e) => handleChange(e)}
+              />
+            </div>
+            <button type="submit" className="btn btn-secondary">
+              Submit
+            </button>
+            <Link to="/" className="btn btn-danger mx-3">
+              Cancel
+            </Link>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default AddTodo
+export default AddTodo;
