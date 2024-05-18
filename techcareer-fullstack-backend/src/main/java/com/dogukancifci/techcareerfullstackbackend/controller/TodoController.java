6e15fabe1ca2a29e05757cb53ccaa3fcd3e726c0
Dogukan Cifci <dogukancifcii5@gmail.com>
2024-05-18 16:29:46 +0300

    GetMapping

--- techcareer-fullstack-backend/src/main/java/com/dogukancifci/techcareerfullstackbackend/controller/TodoController.java
+++ techcareer-fullstack-backend/src/main/java/com/dogukancifci/techcareerfullstackbackend/controller/TodoController.java
@@ -3,9 +3,12 @@ package com.dogukancifci.techcareerfullstackbackend.controller;
 import com.dogukancifci.techcareerfullstackbackend.model.Todo;
 import com.dogukancifci.techcareerfullstackbackend.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController//RESTful web servisi icin (for RESTfuk)
 public class TodoController {
@@ -19,5 +22,10 @@ public class TodoController {
     @PostMapping("/todo")
     Todo newTodo(@RequestBody Todo newTodo) {
         return todoRepository.save(newTodo);
+    }
+    //for get todos list
+    @GetMapping("/todos")
+    List<Todo> getAllTodos() {
+        return todoRepository.findAll();
     }
 }
