b494a4fb87ba1d229189507a93f853dc49a5a388
Dogukan Cifci <dogukancifcii5@gmail.com>
2024-05-18 22:04:07 +0300

    Edit Todo Backend

--- techcareer-fullstack-backend/src/main/java/com/dogukancifci/techcareerfullstackbackend/controller/TodoController.java
+++ techcareer-fullstack-backend/src/main/java/com/dogukancifci/techcareerfullstackbackend/controller/TodoController.java
@@ -22,14 +22,29 @@ public class TodoController {
     Todo newTodo(@RequestBody Todo newTodo) {
         return todoRepository.save(newTodo);
     }
+
     //for get todos list
     @GetMapping("/todos")
     List<Todo> getAllTodos() {
         return todoRepository.findAll();
     }
+
+    //Get by id
     @GetMapping("/todo/{id}")
     Todo getTodoById(@PathVariable Long id) {
         return todoRepository.findById(id)
                 .orElseThrow(() -> new TodoNotFoundException(id)); //bulamazsa exception gonder
+    }
+
+    //Edit
+    @PutMapping("/todo/{id}")
+    Todo updateTodo(@RequestBody Todo newTodo, @PathVariable Long id) {
+        return todoRepository.findById(id)
+                .map(todo -> {
+                    todo.setTitle(newTodo.getTitle());
+                    todo.setContents(newTodo.getContents());
+                    todo.setCompleted(newTodo.isCompleted());
+                    return todoRepository.save(todo);
+                }).orElseThrow(() -> new TodoNotFoundException(id));
     }
 }
